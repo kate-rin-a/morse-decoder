6f2ff92c11631c7cd6c2a6ac274cb812b2be95e2
ka4_ka <klimova_kate@yahoo.com>
2024-10-26 23:11:50 +0300

    feat: add code

--- src/index.js
+++ src/index.js
@@ -38,7 +38,35 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+  let resultArray = [];
+  
+  for (let i = 0; i < expr.length; i+=10) {
+    let part = expr.slice(i, i+10);
+    resultArray.push(part);
+  }
+
+  let newResult = resultArray.map((item) => {
+    let sum = "";
+    if (item === "**********") {
+      return " ";
+    } else {
+        for (let j = 0; j < item.length-1; j+=2 ) {   
+      let code = item.slice(j, j+2);
+      switch(code) {
+        case ("10") : code =  ".";
+        break;
+        case ("11") : code = "-";
+        break;
+        case ("00") : code = "";
+        break; 
+      }
+      sum+=code;
+      }
+    }
+    return MORSE_TABLE[sum];
+    });
+  
+  return newResult.join("");
 }
 
 module.exports = {
